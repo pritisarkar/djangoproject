e7bdcfa55cc0138b12e668d719a3d406b2036469
priti <priti.sarkar@ivanwebsolutions.com>
2024-10-04 15:07:28 +0530

    update

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,5 +3,6 @@ from myapp import views
 urlpatterns = [
     path('createmp/', views.employee,name="createmp"),
     path('fetch/',views.fetch_employee,name="fetch"),
-    path('update/', views.update_employee,name="update")
+    path('update/', views.update_employee,name="update"),
+    path('delete/',views.delete_employee,name="delete")
 ]
